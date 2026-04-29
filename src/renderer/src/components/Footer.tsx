cbaa33be61578c5cbd77a001390dfce130d3d539
Hernán Darío Coronel <dam.hernan.coronel@gmail.com>
2026-04-29 19:50:42 +0200

    updated footer

--- src/renderer/src/components/Footer.tsx
+++ src/renderer/src/components/Footer.tsx
@@ -15,37 +15,41 @@ function Footer(): JSX.Element {
   const pathnames = location.pathname.split('/').filter((x) => x)
 
   return (
-    <Breadcrumb className="bg-green-100 w-full p-1 flex justify-end pr-5">
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink asChild>
-            {pathnames.length !== 0 ? (
-              <Link to="/">Home</Link>
-            ) : (
-              <p className="select-none">Home</p>
-            )}
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        {pathnames.map((value, index) => {
-          const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`
-          const isLast = index === pathnames.length - 1
-          return (
-            <span key={routeTo} className="flex items-center gap-1.5">
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                {isLast ? (
-                  <BreadcrumbPage>{value}</BreadcrumbPage>
-                ) : (
-                  <BreadcrumbLink asChild>
-                    <Link to={routeTo}>{value}</Link>
-                  </BreadcrumbLink>
-                )}
-              </BreadcrumbItem>
-            </span>
-          )
-        })}
-      </BreadcrumbList>
-    </Breadcrumb>
+    <div className="w-full p-1 flex justify-between px-5">
+      <span className="text-sm">v {window.electron?.process.versions.app || '1.0.0'}</span>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              {pathnames.length !== 0 ? (
+                <Link to="/">Home</Link>
+              ) : (
+                <p className="select-none">Home</p>
+              )}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          {pathnames.map((value, index) => {
+            const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`
+            const isLast = index === pathnames.length - 1
+            const capitalizedValue = value[0].toLocaleUpperCase() + value.slice(1)
+            return (
+              <span key={routeTo} className="flex items-center gap-1.5">
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  {isLast ? (
+                    <BreadcrumbPage>{capitalizedValue}</BreadcrumbPage>
+                  ) : (
+                    <BreadcrumbLink asChild>
+                      <Link to={routeTo}>{capitalizedValue}</Link>
+                    </BreadcrumbLink>
+                  )}
+                </BreadcrumbItem>
+              </span>
+            )
+          })}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
   )
 }
 
